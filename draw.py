b7fc1d86284350018ce44f782903edbb5995a0b4
Cem Aksoylar <caksoylar@gmail.com>
2022-04-24 14:24:53 -0700

    Use null values for empty keys and move some validation to model

--- draw.py
+++ draw.py
@@ -6,7 +6,7 @@ from itertools import zip_longest, islice
 from typing import Literal, Optional, Sequence, Mapping, Union
 
 import yaml
-from pydantic import BaseModel, validator
+from pydantic import BaseModel, Field, validator, root_validator
 
 
 KEY_W = 55
@@ -67,16 +67,19 @@ class ComboSpec(BaseModel):
         return Key.from_key_spec(val)
 
 
-KeyRow = Sequence[Key]
+KeyRow = Sequence[Optional[Key]]
 KeyBlock = Sequence[KeyRow]
 
 
 class Layer(BaseModel):
-    left: KeyBlock
-    right: KeyBlock
+    left: KeyBlock = Field(..., alias="keys")
+    right: Optional[KeyBlock]
     left_thumbs: Optional[KeyRow] = None
     right_thumbs: Optional[KeyRow] = None
     combos: Optional[Sequence[ComboSpec]] = None
+
+    class Config:
+        allow_population_by_field_name = True
 
     @validator("left", "right", pre=True)
     def get_key_block(cls, vals):
@@ -93,10 +96,36 @@ class Layout(BaseModel):
     columns: int
     thumbs: Optional[int] = None
 
+    @root_validator
+    def check_thumbs(cls, vals):
+        if vals["thumbs"]:
+            assert vals["thumbs"] <= vals["columns"], "Number of thumbs should not be greater than columns"
+        return vals
+
 
 class KeymapData(BaseModel):
     layout: Layout
     layers: Mapping[str, Layer]
+
+    @root_validator(skip_on_failure=True)
+    def validate_split(cls, vals):
+        if not vals["layout"].split:
+            if any(
+                layer.right is not None or layer.left_thumbs is not None or layer.right_thumbs is not None
+                for layer in vals["layers"].values()
+            ):
+                raise ValueError("Cannot have right or thumb blocks for non-split layouts")
+        return vals
+
+    @root_validator(skip_on_failure=True)
+    def check_combo_pos(cls, vals):
+        total = vals["layout"].rows * vals["layout"].columns * (2 if vals["layout"].split else 1)
+        for layer in vals["layers"].values():
+            if layer.combos:
+                for combo in layer.combos:
+                    assert len(combo.positions) == 2, "Cannot have more than two positions for combo"
+                    assert all(pos < total for pos in combo.positions), "Combo positions exceed number of non-thumb keys"
+        return vals
 
 
 class Keymap:
@@ -105,10 +134,7 @@ class Keymap:
         self.layout = kd.layout
         self.layers = kd.layers
 
-        assert self.layout.split
-        if self.layout.thumbs is not None:
-            assert self.layout.thumbs <= self.layout.columns
-
+        self.total_cols = 2 * self.layout.columns if self.layout.split else self.layout.columns
         self.block_w = self.layout.columns * KEYSPACE_W
         self.block_h = (self.layout.rows + (1 if self.layout.thumbs else 0)) * KEYSPACE_H
         self.layer_w = 2 * self.block_w + OUTER_PAD_W
@@ -146,11 +172,8 @@ class Keymap:
     def print_combo(self, x: float, y: float, combo_spec: ComboSpec) -> None:
         pos_idx = combo_spec.positions
 
-        # no combos with > 2 positions or in thumb keys
-        assert (len(pos_idx) == 2 and
-                all(pos < self.layout.rows * self.layout.columns * (2 if self.layout.split else 1) for pos in pos_idx))
-        cols = [p % ((2 if self.layout.split else 1) * self.layout.columns) for p in pos_idx]
-        rows = [p // ((2 if self.layout.split else 1) * self.layout.columns) for p in pos_idx]
+        cols = [p % self.total_cols for p in pos_idx]
+        rows = [p // self.total_cols for p in pos_idx]
         x_pos = [x + c * KEYSPACE_W + (OUTER_PAD_W if self.layout.split and c >= self.layout.columns else 0) for c in cols]
         y_pos = [y + r * KEYSPACE_H for r in rows]
 
@@ -162,13 +185,15 @@ class Keymap:
     def print_row(self, x: float, y: float, row: KeyRow, is_thumbs: bool = False) -> None:
         assert len(row) == (self.layout.columns if not is_thumbs else self.layout.thumbs)
         lookahead_iter = zip_longest(row, islice(row, 1, None))
-        for key_spec, next_spec in lookahead_iter:
-            if next_spec is not None and key_spec == next_spec and key_spec != "":
-                self.print_key(x, y, key_spec, double=True)
+        for key, next_key in lookahead_iter:
+            if next_key is not None and key == next_key:
+                self.print_key(x, y, key, double=True)
                 x += 2 * KEYSPACE_W
                 _ = next(lookahead_iter)
             else:
-                self.print_key(x, y, key_spec)
+                if key is None:
+                    key = Key(tap="")
+                self.print_key(x, y, key)
                 x += KEYSPACE_W
 
     def print_block(self, x: float, y: float, block: KeyBlock) -> None:
@@ -180,11 +205,12 @@ class Keymap:
     def print_layer(self, x: float, y: float, name: str, layer: Layer) -> None:
         self._draw_text(KEY_W / 2, y - KEY_H / 2, f"{name}:", bold=True)
         self.print_block(x, y, layer.left)
-        self.print_block(
-            x + self.block_w + OUTER_PAD_W,
-            y,
-            layer.right,
-        )
+        if self.layout.split:
+            self.print_block(
+                x + self.block_w + OUTER_PAD_W,
+                y,
+                layer.right,
+            )
         if self.layout.thumbs:
             assert layer.left_thumbs and layer.right_thumbs
             self.print_row(
